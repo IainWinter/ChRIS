0fea2c02e606d5e32393700391dc72ed42c95a51
Sayeed78 <118777033+Sayeed78@users.noreply.github.com>
2023-04-10 13:38:30 -0400

    Uploaded files for Node Status
    Displays prototype of Node Status

---
+++ prototypes/Sohail React/React/src/Testing/Button.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function MyButton() {
+  return (
+    <button>
+      I'm a button
+    </button>
+  );
+}
+
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
+  );
+}
---
+++ prototypes/Sohail React/React/src/edges.js
@@ -0,0 +1,5 @@
+export default [
+  { id: 'e1-2', source: '1', target: '2' },
+  { id: 'e2-3', source: '2', target: '3', animated: true },
+  { id: 'e3-4', source: '3', target: '4' },
+];
